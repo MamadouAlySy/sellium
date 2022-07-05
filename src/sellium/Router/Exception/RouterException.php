965fe4fb5b1c4eabc43b6cbd7ceb0c12e93f4efa
Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
2022-07-05 23:25:02 +0000

    fix: :bug: fix router component

---
+++ src/sellium/Router/Exception/RouterException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sellium\Router;
+
+use Exception;
+
+/**
+* RouterException Class
+*
+* @version 1.0.0
+* @author Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
+*/
+class RouterException extends Exception
+{
+    //
+}
